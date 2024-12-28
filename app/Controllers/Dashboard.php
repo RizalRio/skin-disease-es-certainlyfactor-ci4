4b2f62b49bee83f4450189a539d8d63058fe8de6
Rizal Rio Andrian <rizalrio1357@gmail.com>
2024-12-28 11:23:51 +0700

    dashboard

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -3,14 +3,26 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\Diseases;
+use App\Models\Rules;
+use App\Models\Symptoms;
 use CodeIgniter\HTTP\ResponseInterface;
 
 class Dashboard extends BaseController
 {
     public function index()
     {
+        $diseaseModel   = new Diseases();
+        $symptomModel   = new Symptoms();
+        $rulesModel     = new Rules();
+
         $data = [
-            'title' => 'Dashboard'
+            'title' => 'Dashboard',
+            'data'  => [
+                'totalDisease'  => $diseaseModel->countAll(),
+                'totalSymptom'  => $symptomModel->countAll(),
+                'totalRules'    => $rulesModel->countAll()
+            ]
         ];
 
         return view('pages/dashboard', $data);
--- app/Views/pages/dashboard.php
+++ app/Views/pages/dashboard.php
@@ -5,16 +5,41 @@
     <div class="row">
         <div class="col-lg-3 col-6">
             <!-- small box -->
-            <div class="small-box bg-info">
+            <div class="small-box bg-success">
                 <div class="inner">
-                    <h3>150</h3>
+                    <h3><?= $data['totalDisease'] ?></h3>
 
-                    <p>New Orders</p>
+                    <p>Penyakit</p>
                 </div>
                 <div class="icon">
-                    <i class="ion ion-bag"></i>
+                    <i class="fas fa-disease"></i>
                 </div>
-                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+        <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-info">
+                <div class="inner">
+                    <h3><?= $data['totalSymptom'] ?></h3>
+
+                    <p>Gejala</p>
+                </div>
+                <div class="icon">
+                    <i class="fas fa-star-of-life"></i>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-primary">
+                <div class="inner">
+                    <h3><?= $data['totalRules'] ?></h3>
+
+                    <p>Rules</p>
+                </div>
+                <div class="icon">
+                    <i class="fas fa-balance-scale"></i>
+                </div>
             </div>
         </div>
     </div>
